db364b81abc51cba39ecd26bf6dd26ce38f02347
kdrl <kdrl7@naver.com>
2017-03-17 21:30:39 +0900

    vr movemovement added

---
+++ Eat-Heart-VR/Eat-Heart/Assets/PTMovementMultiply.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+public var scale: Number = 3;
+
+function Start () {
+	Debug.Log("This is dubug message.");
+}
+// Frameごとに呼ばれる関数？
+function FixedUpdate () {
+	//var position = UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.CenterEye);
+
+	//Debug.Log(position);
+
+	//transform.position = position * scale;
+}
+
+function Update () {
+	var position = UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.CenterEye);
+
+	Debug.Log(scale);
+	var cameraTransform = GetComponent.<Transform>();
+	cameraTransform.position = position * scale;
+	Debug.Log(cameraTransform.position);
+}
