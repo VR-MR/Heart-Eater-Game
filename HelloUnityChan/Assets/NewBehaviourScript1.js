a8969848c93f5a0606db32688e8e0912b9a69851
飯沼　俊平 <01014452@CA1575.local>
2017-03-17 21:23:10 +0900

    add actors

---
+++ HelloUnityChan/Assets/NewBehaviourScript1.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+private var rb: Rigidbody;
+
+function getPlayerPosition() {
+	return Camera.main.gameObject.transform.position;
+}
+
+function randomPosition(center, xVar, yVar) {
+//	return Vector3(center.x + xVar * Math.random(), center.y + yVar * Math.random(), center.z);
+}
+
+function Start () {
+//	pos = getPlayerPosition();
+//	print(pos);
+//	var targetPos = randomPosition(pos, 0.5, 0.5);
+//	print(targetPos);
+//
+//	rb = GetComponent.<Rigidbody>();
+//	var movement = Vector3(0, 200, -200);
+//	rb.AddForce (movement);
+}
+
+
+function Update () {
+
+}
